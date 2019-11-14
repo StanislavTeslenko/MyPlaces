afd35d3c632e91eece5bbf2137b54927f8f7ed4d
Stanislav Teslenko <stanislav.teslenko@gmail.com>
2019-11-14 14:55:07 +0200

    add custom cell

---
+++ MyPlaces/Cells/CustomTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Stanislav Teslenko on 14.11.2019.
+//  Copyright © 2019 Stanislav Teslenko. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    
+    
+    
+
+}
